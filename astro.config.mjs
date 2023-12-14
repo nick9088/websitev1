ad7ad2014e53a52af3bf8f499e8d1226d9511504
nick909 <princessblair.cm@gmail.com>
2023-12-14 22:42:01 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321',
+  site: 'https://nick909.dev',
   integrations: [mdx(), sitemap(), tailwind()]
 });
